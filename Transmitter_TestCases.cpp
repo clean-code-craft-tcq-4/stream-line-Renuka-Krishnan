7c94ae449e97e4b627ab70dfde895b18ce1fdd05
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-09-13 15:11:58 +0530

    Update Transmitter_TestCases.cpp

--- Transmitter_TestCases.cpp
+++ Transmitter_TestCases.cpp
@@ -1,1 +1,10 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
+#include "test/catch.hpp"
+#include "Transmitter.h"
+
+TEST_CASE("Test to verify if the file is opened successfully") 
+{
+ FILE *fp = fopen("./BMS_Sensor_Data.txt", "r"); 
+ REQUIRE_NOTHROW(ReadSensorData()== OK); 
+}
