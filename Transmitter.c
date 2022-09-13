b5e19fe2a23e9e266e9517784a82ce8277fa7d66
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-09-13 17:56:31 +0530

    Update Transmitter.c

--- Transmitter.c
+++ Transmitter.c
@@ -6,8 +6,8 @@
 Ret_Status Return1,  Return2;
 int BufferSize = 0;
 
-int BatteryTemp[50]={0};
-int BatterySoc[50]={0};
+int BatteryTemp[45]={0};
+int BatterySoc[45]={0};
    
 Ret_Status ReadSensorData()
 {
