019981d5b9cef7061adc0e7da9238c703ee83e1b
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-09-13 20:40:25 +0530

    Update Transmitter.h

--- Transmitter.h
+++ Transmitter.h
@@ -16,6 +16,10 @@ typedef enum
   StartTransmit
 } Modes;
 
+Ret_Status Return1,  Return2;
+int BufferSize = 0;
+int BatteryTemp[45]={0};
+int BatterySoc[45]={0};
 
 Ret_Status ReadSensorData();
 Ret_Status PrintOnConsole(Modes TransmissionRequest);
