2a861ba5de21764293f75ab88cdb6e364e1a8924
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-09-13 16:48:45 +0530

    Update Transmitter.c

--- Transmitter.c
+++ Transmitter.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include "Transmitter.h"
 
+Ret_Access Return1,  Return2;
+int BufferSize = 0;
+
+int BatteryTemp[]={};
+int BatterySoc[]={};
+   
+Ret_Status ReadSensorData()
+{
+  FILE *fp;
+  int Buffer = 1; 
+  Return1=NOK;   	
+  fp = fopen("./BMS_Sensor_Data.txt", "r");
+  
+  if (fp != NULL)
+  {
+      int ReadTemp, ReadSoc, Index =0;
+      while(Buffer != EOF)
+      {
+       Buffer = fscanf(fp,"%d %d",&ReadTemp, &ReadSoc);
+       BatteryTemp[Index] =ReadTemp;   
+       BatterySoc[Index]= ReadSoc;
+       Index++;
+      }
+       BufferSize=Index;
+       Return1= OK;
+  }	
+ fclose(fp);
+ return Return1;
+}
