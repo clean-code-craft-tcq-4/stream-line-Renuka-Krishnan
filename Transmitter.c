f4f5b0842544957266efff312e6e851268e2cdeb
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-10-31 10:12:49 +0530

    Update Transmitter.c

--- Transmitter.c
+++ Transmitter.c
@@ -23,6 +23,7 @@ Ret_Status ReadSensorData()
        data = fscanf(fp,"%d %d",&ReadTemp, &ReadSoc);
        BatteryTemp[Index] =ReadTemp;   
        BatterySoc[Index]= ReadSoc;
+        calculate_battery_health(BatteryTemp,BatterySoc,Index);
        Index++;
       }
        BufferSize=Index;
