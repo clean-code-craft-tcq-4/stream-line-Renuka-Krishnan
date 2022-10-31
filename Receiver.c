64069bdc7d7d60862b3f47c6bf05e35dd24d08fb
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-10-31 12:27:47 +0530

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -10,19 +10,60 @@ void calculate_battery_health(int bat_temp,int bat_soc,int counter)
   battery_state_of_charge[counter] = bat_soc;
   if(battery_state_of_charge[counter] < 20)
   {
-    if( battery_temperature[counter] > 50)
+    if( battery_temperature[counter] > 40 || battery_temperature[counter] < 0 )
     {
       printf("Battery is in bad condition%d,%d\n", battery_temperature[counter],battery_state_of_charge[counter]);
+    }
+    if( battery_temperature[counter] < 40 && battery_temperature[counter] > 0 )
+    {
+      printf("Battery is in good condition%d,%d\n", battery_temperature[counter],battery_state_of_charge[counter]);
     }
   }
   
    if(battery_state_of_charge[counter] >20 && battery_state_of_charge[counter] <40)
   {
-    if( battery_temperature[counter] > 50)
+        if( battery_temperature[counter] > 40 || battery_temperature[counter] < 0 )
     {
-      printf("Battery is going condition%d,%d\n", battery_temperature[counter],battery_state_of_charge[counter]);
+      printf("Battery is in bad condition%d,%d\n", battery_temperature[counter],battery_state_of_charge[counter]);
+    }
+    if( battery_temperature[counter] < 40 && battery_temperature[counter] > 0 )
+    {
+      printf("Battery is in good condition%d,%d\n", battery_temperature[counter],battery_state_of_charge[counter]);
     }
   }
   
+  if(battery_state_of_charge[counter] >40 && battery_state_of_charge[counter] <60)
+  {
+        if( battery_temperature[counter] > 40 || battery_temperature[counter] < 0 )
+    {
+      printf("Battery is in bad condition%d,%d\n", battery_temperature[counter],battery_state_of_charge[counter]);
+    }
+    if( battery_temperature[counter] < 40 && battery_temperature[counter] > 0 )
+    {
+      printf("Battery is in good condition%d,%d\n", battery_temperature[counter],battery_state_of_charge[counter]);
+    }
+  }
+  if(battery_state_of_charge[counter] >60 && battery_state_of_charge[counter] <80)
+  {
+       if( battery_temperature[counter] > 40 || battery_temperature[counter] < 0 )
+    {
+      printf("Battery is in bad condition%d,%d\n", battery_temperature[counter],battery_state_of_charge[counter]);
+    }
+    if( battery_temperature[counter] < 40 && battery_temperature[counter] > 0 )
+    {
+      printf("Battery is in good condition%d,%d\n", battery_temperature[counter],battery_state_of_charge[counter]);
+    }
+  }
+  if(battery_state_of_charge[counter] >80)
+  {
+          if( battery_temperature[counter] > 40 || battery_temperature[counter] < 0 )
+    {
+      printf("Battery is in bad condition%d,%d\n", battery_temperature[counter],battery_state_of_charge[counter]);
+    }
+    if( battery_temperature[counter] < 40 && battery_temperature[counter] > 0 )
+    {
+      printf("Battery is in good condition%d,%d\n", battery_temperature[counter],battery_state_of_charge[counter]);
+    }
+  }
   
 }
