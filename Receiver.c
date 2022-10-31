3f6ac2439cfa0e4f2549d8b90b2da03135001e3c
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-10-31 10:12:54 +0530

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include "Transmitter.h"
+int battery_temperature[50],battery_state_of_charge[50];
 
+void calculate_battery_health(int bat_temp,int bat_soc,int counter)
+{
+  battery_temperature[counter] = bat_temp;
+  battery_state_of_charge[counter] = bat_soc;
+  printf("%d,%d\n", battery_temperature[counter],battery_state_of_charge[counter]);
+}
